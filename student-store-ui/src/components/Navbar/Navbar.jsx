c1a6eb7b9aafc01727f7b103d18d108263ebfaaf
Chris Barfield <cbarfield@fb.com>
2022-06-21 23:59:30 -0700

    Initial commit

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,106 @@
-import * as React from "react"
-import "./Navbar.css"
+import * as React from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <header>
+        <nav class="hidden md:block navbar navbar-expand-lg shadow-md py-5 bg-white relative items-center w-full justify-between">
+          <div class="px-6 w-full flex flex-wrap items-center justify-between ">
+            <div class="flex items-center"></div>
+            <div
+              class="navbar-collapse collapse grow items-center"
+              id="navbarSupportedContentY"
+            >
+              <ul class="navbar-nav mr-auto md:flex lg:flex lg:flex-row">
+                <li class="nav-item">
+                  <a
+                    class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item mb-2 lg:mb-0">
+                  <a
+                    class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div class="text-center bg-gray-50 text-gray-800 py-20 px-6">
+          <h1 class="text-5xl font-bold mt-0 mb-6">Student Store</h1>
+          <form>
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search"
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </header>
     </nav>
-  )
+  );
 }
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,42 @@
-import * as React from "react"
-import "./Sidebar.css"
+import * as React from "react";
+import { useState } from "react";
+import "./Sidebar.css";
 
 export default function Sidebar() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <section className="sidebar">
-      <p>Sidebar</p>
+      {showSidebar ? (
+        <button
+          className="flex text-4xl text-white items-center cursor-pointer fixed right-10 top-6 z-50"
+          onClick={() => setShowSidebar(!showSidebar)}
+        >
+          x
+        </button>
+      ) : (
+        <svg
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
+          fill="#2563EB"
+          viewBox="0 0 100 80"
+          width="40"
+          height="40"
+        >
+          <rect width="100" height="10"></rect>
+          <rect y="30" width="100" height="10"></rect>
+          <rect y="60" width="100" height="10"></rect>
+        </svg>
+      )}
+
+      <div
+        className={`top-0 right-0 w-[35vw] bg-gray-800  p-10 pl0 text-white fixed h-full z-40  ease-in-out duration-300 ${
+          showSidebar ? "translate-x-0 " : "translate-x-full"
+        }`}
+      >
+        <h3 className="mt-20 text-4xl font-semibold text-white">
+          Sidebar Content
+        </h3>
+      </div>
     </section>
-  )
+  );
 }
